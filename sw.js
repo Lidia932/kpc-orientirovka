2e85cac5547d4f4e62c1e54581f7cb2427e89919
Lidia932 <alekseeva.li@roistat.com>
2026-08-14 12:34:02 +0400

    Bump cache for special marks wrapping

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='kpc-orientirovka-v8';
+const CACHE='kpc-orientirovka-v9';
 const ASSETS=['./','./index.html','./styles.css','./app-v5.js','./manifest.webmanifest','./template.jpg','./icon.svg'];
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));
