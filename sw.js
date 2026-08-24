d295aa4aec714b0187f3ad74e1edb1cf7df5bdca
Lidia932 <alekseeva.li@roistat.com>
2026-08-24 15:33:31 +0400

    Hide install action for installed app

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='kpc-orientirovka-v18';
-const ASSETS=['./','./index.html','./styles.css','./app-v5.js?v=18','./firebase-config.js','./manifest.webmanifest','./template.jpg','./icon.svg','./icon-192.png','./icon-512.png'];
+const CACHE='kpc-orientirovka-v19';
+const ASSETS=['./','./index.html','./styles.css','./app-v5.js?v=19','./firebase-config.js','./manifest.webmanifest','./template.jpg','./icon.svg','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));
 });
