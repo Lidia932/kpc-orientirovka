c7b2611ec9117fa3258d119f4c3f3731db283da9
Lidia932 <alekseeva.li@roistat.com>
2026-08-24 21:57:28 +0400

    Hide auth form during session restore

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='kpc-orientirovka-v42';
-const ASSETS=['./','./index.html','./styles.css?v=42','./app-v5.js?v=42','./firebase-config.js','./manifest.webmanifest','./template.jpg','./icon.svg','./icon-192.png','./icon-512.png','./vendor/jspdf.umd.min.js'];
+const CACHE='kpc-orientirovka-v43';
+const ASSETS=['./','./index.html','./styles.css?v=43','./app-v5.js?v=43','./firebase-config.js','./manifest.webmanifest','./template.jpg','./icon.svg','./icon-192.png','./icon-512.png','./vendor/jspdf.umd.min.js'];
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));
 });
