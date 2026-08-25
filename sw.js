8383a87c92f8cb90d69b239e101ed7d7450953fe
Lidia932 <alekseeva.li@roistat.com>
2026-08-25 10:35:09 +0400

    Add track opening app choices

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE='kpc-orientirovka-v55';
+const CACHE='kpc-orientirovka-v56';
 const SHARE_CACHE='kpc-shared-tracks-v1';
-const ASSETS=['./','./index.html','./styles.css?v=55','./app-v5.js?v=55','./firebase-config.js','./manifest.webmanifest?v=55','./template.jpg','./icon.svg','./icon-192.png','./icon-512.png','./vendor/jspdf.umd.min.js'];
+const ASSETS=['./','./index.html','./styles.css?v=56','./app-v5.js?v=56','./firebase-config.js','./manifest.webmanifest?v=56','./template.jpg','./icon.svg','./icon-192.png','./icon-512.png','./vendor/jspdf.umd.min.js'];
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));
 });
